614a3c0ff6f04cfcf2525d44572a6c8ac2b9e3b8
Özkan Yilmaz <ozkanyilmaz@gmail.com>
2018-08-25 18:12:13 +0300

    Persisting data using Codable protocol and Plist file

--- Listem/AppDelegate.swift
+++ Listem/AppDelegate.swift
@@ -15,8 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        print("didFinishLaunchingWithOptions")
-        // Override point for customization after application launch.
+        
+                
+        
         return true
     }
 
